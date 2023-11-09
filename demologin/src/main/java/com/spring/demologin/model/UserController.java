f01598cedc22556f8d8fe8b6fed3fb97217f0bc6
Beyhan Kandemir <105010007+Beykn@users.noreply.github.com>
2023-11-09 15:21:42 +0300

    change the user-form-html format ,write country in the application.properties and with the @Value annotation we can acces this in UserController.

--- demologin/src/main/java/com/spring/demologin/model/UserController.java
+++ demologin/src/main/java/com/spring/demologin/model/UserController.java
@@ -1,13 +1,19 @@
 package com.spring.demologin.model;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class UserController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/login")
     public String form(Model theModel){
